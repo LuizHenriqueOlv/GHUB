bbc24e4f6d1782a10c00825cf07b7b6a8f17757c
20231TIIMI0260 <luizhenrique.oliveiradahora@gmail.com>
2025-11-25 09:49:47 -0300

    feat: adicionando dependencia de authenticação

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation("com.google.firebase:firebase-auth")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
